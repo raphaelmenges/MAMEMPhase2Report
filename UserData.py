350a2407d6689b7e6d9496db0091c13e83798e1d
Raphael Menges <unrealer2@yahoo.de>
2018-03-12 12:08:06 +0100

    Total active hours.

--- UserData.py
+++ UserData.py
@@ -15,12 +15,13 @@ class UserData():
 		
 	# Selfreport
 	def self_report(self):
-		rp.print_line("Nickname: ", self.get_data(Keys.nickname))
-		rp.print_line("Starts: ", str(self.get_data(Keys.start_count)))
-		rp.print_line("Last Start: ", str(hlp.to_date(self.get_data(Keys.start_list, self.get_data(Keys.start_count)-1, 'date'))))
+		rp.print_line("Nickname: ", self._get_data(Keys.nickname))
+		rp.print_line("Starts: ", str(self._get_data(Keys.start_count)))
+		rp.print_line("Latest Start: ", str(hlp.to_date(self._get_data(Keys.start_list, self._get_data(Keys.start_count)-1, 'date'))))
+		rp.print_line("Total Active Hours: ", str(self._total_active_hours()))
 		
 	# Get data by key path into nested dict structure. Accepts additional custom keys
-	def get_data(self, key_path, *args):
+	def _get_data(self, key_path, *args):
 		copy_path = list(key_path) # copy keys
 		for arg in args: # append custom keys
 			copy_path.append(str(arg))
@@ -28,4 +29,17 @@ class UserData():
 	
 	# Comparator for sorting
 	def __lt__(self, other):
-		return self.get_data(Keys.nickname) < other.get_data(Keys.nickname)
+		return self._get_data(Keys.nickname) < other._get_data(Keys.nickname)
+	
+	### Further calculations ###
+	
+	# Total time in front of eye tracker
+	def _total_active_hours(self):
+		total_active_hours = 0.0
+		if 'pageActivity' in self._data:
+			for page, activity in self._data['pageActivity'].items():
+				for session in activity['sessions']:
+					total_active_hours += session['durationUserActive'] / (60.0 * 60.0) # from seconds to hours
+		return total_active_hours
+	
+	####################
