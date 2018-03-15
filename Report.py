e8b0450400d866045d73b3396a9702ff5e05874c
Raphael Menges <unrealer2@yahoo.de>
2018-03-15 08:14:53 +0100

    Output folder is generated by Python.

--- Report.py
+++ Report.py
@@ -1,6 +1,12 @@
 import Defines as dfn
+import os
 
 def init_file():
+	
+	# Create the output folder if necessary
+	if not os.path.exists(dfn.output_dir):
+		os.makedirs(dfn.output_dir)
+	
 	with open(dfn.output_dir + 'report.txt', 'w') as f:
 		f.write('############################ \n') # Header
 		f.write('### MAMEM Phase 2 Report ### \n') # Header
