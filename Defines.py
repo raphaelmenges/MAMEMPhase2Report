698d81e93cb049a999ffd2809ef76c169b8c3fd6
Raphael Menges <unrealer2@yahoo.de>
2018-03-27 10:49:29 +0200

    New users.

--- Defines.py
+++ Defines.py
@@ -11,11 +11,17 @@ plot_format = ".pdf"
 # Tuple of nickname and date of setup
 User = collections.namedtuple('User', 'nickname setup_date')
 user_filter = [
+		
+		# Sheba users
 		User('sheba1', 	datetime.datetime(2018, 2, 28, 9,  0,  0)),
 		User('sheba2', 	datetime.datetime(2018, 3, 2,  9,  0,  0)),
 		User('sheba3', 	datetime.datetime(2018, 3, 1,  13, 0,  0)),
 		User('sheba4', 	datetime.datetime(2018, 3, 8,  13, 0,  0)),
 		User('sheba5', 	datetime.datetime(2018, 3, 6,  18, 0,  0)),
+		User('Timor', 		datetime.datetime(2018, 3, 25, 12, 30, 0)), # sheba6 -> think about mapping from "custom nicknames" to something like "user id". Or change nickname later back to sheba6
+		User('sheba7', 	datetime.datetime(2018, 3, 26, 13, 0,  0)),
+		
+		# MDA users
 		User('mda1', 		datetime.datetime(2018, 3, 6,  11, 0,  0)),
 		User('mda2', 		datetime.datetime(2018, 3, 7,  10, 0,  0)),
 		User('mda3', 		datetime.datetime(2018, 3, 8,  11, 0,  0)),
