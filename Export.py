143900c5d8ecc9685ffbbefab83f52c867b3dbcb
Raphael Menges <unrealer2@yahoo.de>
2018-04-25 19:01:13 +0200

    Total active hours export.

--- Export.py
+++ Export.py
@@ -4,7 +4,7 @@ import csv
 
 # Export accumulated data per user
 def accumulated_data_per_user(user_data_list):
-	fieldnames = ['id', 'start_count', 'run_time_hours']
+	fieldnames = ['id', 'start_count', 'run_time_hours', 'total_active_hours']
 	with open(dfn.output_dir + 'users.csv', 'w', newline='') as csvfile:
 		output = csv.DictWriter(csvfile, delimiter=',', quoting=csv.QUOTE_MINIMAL, fieldnames=fieldnames)
 		output.writeheader()
@@ -13,7 +13,7 @@ def accumulated_data_per_user(user_data_list):
 		for user_data in user_data_list:
 			
 			# Write user data
-			output.writerow({'id': user_data.mid, 'start_count': user_data.start_count, 'run_time_hours': user_data.total_run_time_hours})
+			output.writerow({'id': user_data.mid, 'start_count': user_data.start_count, 'run_time_hours': user_data.total_run_time_hours, 'total_active_hours': user_data.total_active_hours})
 			
 # Export different daily use metrics per user
 def daily_use_per_user(user_data_list):
