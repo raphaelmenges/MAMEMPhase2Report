6a41add2868edb41ad5fc69da806efe66d1fe485
Raphael Menges <unrealer2@yahoo.de>
2019-01-28 11:35:47 +0100

    Accumulated data.

--- __main__.py
+++ __main__.py
@@ -39,6 +39,48 @@ with open('mamem-phase2-fall17-export.json', encoding='utf-8') as data_file:
 	# General information
 	rp.print_line('Generation Date: ', datetime.datetime.now())
 	rp.print_line('User Count: ', len(user_data_list))
+	
+	# Accumulate data over all users
+	run_time_hours = 0.0
+	active_hours = 0.0
+	page_count = 0
+	char_count = 0
+	click_count = 0
+	pause_count = 0
+	tab_switching_count = 0
+	url_input_count = 0
+	recalibration_count = 0
+	bookmark_adding_count = 0
+	for user_data in user_data_list:
+		
+		# Go over daily use
+		for day, use in user_data.daily_use.items():
+			active_hours += use['general']['active_hours']
+			page_count += use['general']['page_count']
+			char_count += use['general']['char_input_count']
+			click_count += use['general']['click_count']
+			
+		# General
+		run_time_hours += user_data.total_run_time_hours
+		pause_count += user_data.pause_count
+		tab_switching_count += user_data.tab_switching_count
+		url_input_count += user_data.url_input_count
+		bookmark_adding_count += user_data.bookmark_adding_count
+		
+		# Recalibration
+		for item in user_data.recalibrations_per_start:
+			recalibration_count += item[1]
+	
+	rp.print_line('Run Time Hours: ', run_time_hours)
+	rp.print_line('Active Hours: ', active_hours)
+	rp.print_line('Page Count: ', page_count)
+	rp.print_line('Char Input Count: ', char_count)
+	rp.print_line('Click Count: ', click_count)
+	rp.print_line('Pause Count: ', pause_count)
+	rp.print_line('Tab Switching Count: ', tab_switching_count)
+	rp.print_line('URL Input Count: ', url_input_count)
+	rp.print_line('Recalibration Count: ', recalibration_count)
+	rp.print_line('Bookmark Adding Count: ', bookmark_adding_count)
 	rp.print_line('')
 	
 	# Individual information
@@ -67,8 +109,8 @@ with open('mamem-phase2-fall17-export.json', encoding='utf-8') as data_file:
 	# print('.', end='')
 	# plt.daily_use(user_data_list)
 	# print('.', end='')
-	# plt.general_metrics_counts(user_data_list)
-	# print('.', end='')
+	plt.general_metrics_counts(user_data_list)
+	print('.', end='')
 	plt.normalized_daily_use(user_data_list)
 	print('.', end='')
 	# plt.accumulated_normalized_daily_use(user_data_list)
@@ -79,8 +121,8 @@ with open('mamem-phase2-fall17-export.json', encoding='utf-8') as data_file:
 	print('Export', end='')
 	# ex.accumulated_data_per_user(user_data_list)
 	# print('.', end='')
-	# ex.daily_use_per_user(user_data_list)
-	# print('.', end='')
+	ex.daily_use_per_user(user_data_list)
+	print('.', end='')
 	ex.domain_infos(user_data_list)
 	print('.', end='')
 	print('finished.')
